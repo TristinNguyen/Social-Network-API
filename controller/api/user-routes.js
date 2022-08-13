0d5ac4637fb1cc8faf5e37641e6879c496fb1bea
Tristin Nguyen <nguye501@msu.edu>
2022-08-13 12:51:16 -0400

    testing routes and functions

--- controller/api/user-routes.js
+++ controller/api/user-routes.js
@@ -4,8 +4,8 @@ const router = require('express').Router();
 router.route('/')
     .get((req, res) => {
         User.find({})
-        .populate({path: 'thoughts', select: '-__v'})
-        .populate({path: 'friends', select: '-__v'})
+        // .populate({path: 'thoughts', select: '-__v'})
+        // .populate({path: 'friends', select: '-__v'})
         .select('-__v')
         .then(dbUserData => res.json(dbUserData))
         .catch(err => {
@@ -32,7 +32,7 @@ router.route('/:id')
         .catch(err => res.status(400).json(err))
     })
     
-    .put((req, res) => {
+    .put(({params, body}, res) => {
         User.findOneAndUpdate(
             {_id: params.id}, body,
             {new: true, runValidators: true}
